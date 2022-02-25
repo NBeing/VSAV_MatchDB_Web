670d78d871ed053427aaa3fa34343dbc961dfe1d
N-Bee <jeffrey.m.rivera@gmail.com>
2022-02-25 09:25:46 -0500

    * Add Jest / RTL

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  modulePaths: ["src"],
+  rootDir : "./src",
+  moduleNameMapper: {
+      "^@Components(.*)$"     : "<rootDir>/components$1",
+      "^@Pages(.*)$"          : "<rootDir>/components/pages/$1",
+      "^@MatchService(.*)$"   : "<rootDir>/services/matches$1",
+      "^@AuthService(.*)$"    : "<rootDir>/services/auth$1",
+      "^@Services(.*)$"       : "<rootDir>/services$1",
+      "^@CommonEnums(.*)$"    : "<rootDir>/common/enums$1",
+      "^@Common(.*)$"         : "<rootDir>/common$1",
+
+  }
+};
