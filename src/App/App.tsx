24be1986df9f37006a1a776bbeb45b8ba44961df
N-Bee <jeffrey.m.rivera@gmail.com>
2022-02-26 13:22:09 -0500

    * Move App.tsx

---
+++ src/App/App.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import {
+    Routes,
+    Route,
+    Link,
+    Outlet
+} from "react-router-dom";
+
+import { RequireAuth } from "@Components/auth/RequireAuth";
+import Login from "@Pages/login/Login.component";
+import { NavBar } from "@Components/navigation/NavBar.component";
+import { MatchList } from "@Pages/matchlist/MatchList.component";
+import AddMatch from "@Pages/addmatch/AddMatch.component";
+
+export default function App() {
+  return (
+    <div data-testid="app-wrapper">
+      <NavBar/>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<MatchList />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/protected"
+            element={
+              <RequireAuth>
+                <AddMatch />
+              </RequireAuth>
+            }
+          />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/protected">Protected Page (Test Auth)</Link>
+        </li>
+      </ul>
+
+      <Outlet />
+    </div>
+  );
+}
+
+
+
