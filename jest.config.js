9d4303752bbc3eb23af2d5bf8241466716374555
N-Bee <jeffrey.m.rivera@gmail.com>
2022-02-26 13:19:28 -0500

    * Add new aiases to tsconfig, jest config

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,12 @@ module.exports = {
   rootDir : "./src",
   moduleNameMapper: {
       "^@Components(.*)$"     : "<rootDir>/components$1",
-      "^@Pages(.*)$"          : "<rootDir>/components/pages/$1",
+      "^@Pages(.*)$"          : "<rootDir>/components/pages$1",
       "^@MatchService(.*)$"   : "<rootDir>/services/matches$1",
       "^@AuthService(.*)$"    : "<rootDir>/services/auth$1",
       "^@Services(.*)$"       : "<rootDir>/services$1",
       "^@CommonEnums(.*)$"    : "<rootDir>/common/enums$1",
       "^@Common(.*)$"         : "<rootDir>/common$1",
-
+      "^@Root(.*)$"           : "<rootDir>$1"
   }
 };
