629fa10170b498087d9d8c14a6186cfd9a77fc1b
N-Bee <jeffrey.m.rivera@gmail.com>
2022-02-26 11:46:22 -0500

    * MatchInfoService add error handling

--- src/services/matches/MatchInfo.service.ts
+++ src/services/matches/MatchInfo.service.ts
@@ -1,5 +1,6 @@
 import api from "@Common/http.common";
 import IMatchData from "@MatchService/MatchData.type"
+import { AxiosError } from "axios";
 
 const BASE_URL = `/vsav_info/matches/`
 
@@ -18,36 +19,57 @@ export const EMPTY_MATCHLIST = {
   previous: null,
   results: []
 }
+
 class MatchInfoService {
 
   async getPage(page_num: number): Promise<MatchListResponse> {
-    const endpoint = BASE_URL + `?page=${page_num}`
-    const { data } = await api.get<MatchListResponse>(endpoint)
-    return data 
+    try {
+      const endpoint = BASE_URL + `?page=${page_num}`
+      const { data } = await api.get<MatchListResponse>(endpoint)
+      return data 
+    } catch (e) {
+      throw new Error(JSON.stringify(e))
+    }
   }
 
   async getById(id:string): Promise<IMatchData> {
-    const endpoint = `${BASE_URL}${id}`
-    const { data } = await api.get<IMatchData>(endpoint);
-    return data
+    try {
+      const endpoint = `${BASE_URL}${id}`
+      const { data } = await api.get<IMatchData>(endpoint);
+      return data
+    } catch (e) {
+      throw new Error(JSON.stringify(e))
+    }
   }
 
   async create(match: IMatchData): Promise<IMatchData> {
-    const endpoint = `${BASE_URL}`
-    const { data } = await api.post<IMatchData>(endpoint, match);
-    return data
+    try {
+      const endpoint = `${BASE_URL}`
+      const { data } = await api.post<IMatchData>(endpoint, match);
+      return data
+    } catch (e) {
+      throw new Error(JSON.stringify(e))
+    }
   }
 
   async update(match: IMatchData, id: string): Promise<IMatchData> {
-    const endpoint = `${BASE_URL}${id}/`
-    const { data } = await api.put<IMatchData>(endpoint, match);
-    return data
+    try {
+      const endpoint = `${BASE_URL}${id}/`
+      const { data } = await api.put<IMatchData>(endpoint, match);
+      return data
+    } catch (e) {
+      throw new Error(JSON.stringify(e))
+    }
   }
 
   async delete(id:string): Promise<IMatchData> {
-    const endpoint = `${BASE_URL}${id}`
-    const { data } = await api.delete<IMatchData>(endpoint);
-    return data
+    try {
+      const endpoint = `${BASE_URL}${id}`
+      const { data } = await api.delete<IMatchData>(endpoint);
+      return data
+    } catch (e) {
+      throw new Error(JSON.stringify(e))
+    }
   }
 }
 export default new MatchInfoService();
