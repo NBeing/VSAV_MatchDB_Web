54d9a5318b94dbba39b85de9da0ede8b68039df7
N-Bee <jeffrey.m.rivera@gmail.com>
2022-02-27 12:19:23 -0500

    * Add navbar stying stub

--- src/components/navigation/NavBar.component.tsx
+++ src/components/navigation/NavBar.component.tsx
@@ -1,14 +1,44 @@
 import React, {ReactElement} from "react"
-import { AuthStatus } from "@Components/navigation/AuthStatus.component"
 import { Link } from "react-router-dom"
-import { ROUTE_NAMES } from '@Routes/RouteNames.const';
 
-export const NavBar:React.FC = ():ReactElement => {
+import { ROUTE_NAMES } from '@Routes/RouteNames.const';
+import { AuthStatus } from "@Components/navigation/AuthStatus.component"
+
+import {createUseStyles, useTheme} from 'react-jss'
+import type { CustomTheme } from '@Theme/Theme'
+
+type RuleNames = 
+  'wrapper'  |
+  'link'
+
+const useStyles = createUseStyles<RuleNames, NavBarProps, CustomTheme>({
+  wrapper: ({theme}) => ({
+    background: theme.background || 'black'
+  }),
+  link:  {},
+})
+
+interface NavBarProps {}
+
+export const NavBar:React.FC = ({...props}: NavBarProps):ReactElement => {
+
+    const theme:CustomTheme = useTheme<CustomTheme>()
+    const classes = useStyles({...props, theme})
+
     return (
-        <div>
-            <Link to={`/${ROUTE_NAMES.LOGIN}`}>Login</Link>
-            <Link to={`/${ROUTE_NAMES.ADD_MATCH}`}>Add Match (login req)</Link>
-            <Link to={`/${ROUTE_NAMES.LISTING}`}>Match List</Link>
+        <div
+            data-testid='NavBar-container'
+            className={classes.wrapper}
+        >
+            <Link className="link" to={`/${ROUTE_NAMES.LOGIN}`}>
+                Login
+            </Link>
+            <Link className="link" to={`/${ROUTE_NAMES.ADD_MATCH}`}>
+                Add Match (login req)
+            </Link>
+            <Link className="link" to={`/${ROUTE_NAMES.LISTING}`}>
+                Match List
+            </Link>
             <AuthStatus />
         </div>
     )
