e294e2fb72e706d38a7e02b918ec7f2a0a2fc769
N-Bee <jeffrey.m.rivera@gmail.com>
2022-02-26 19:26:28 -0500

    * update matchlist mock resp

--- src/components/pages/matchlist/__test__/MatchList.resp.tsx
+++ src/components/pages/matchlist/__test__/MatchList.resp.tsx
@@ -31,48 +31,6 @@ const initialPage = {
             "video_title": "",
             "uploader": "TESTER"
         },
-        {
-            "id": "43c06f03-b42b-495c-b6b5-91fcf5bc20c1",
-            "type": "VI",
-            "url": "https://youtu.be/Cm9UTXJZgzw",
-            "p1_char": "FE",
-            "p2_char": "GA",
-            "winning_char": "",
-            "p1_name": "",
-            "p2_name": "",
-            "timestamp": 279,
-            "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
-            "uploader": "TESTER"
-        },
-        {
-            "id": "e8ccb825-cd84-4741-988b-6a8536fe3bdc",
-            "type": "VI",
-            "url": "https://youtu.be/Cm9UTXJZgzw",
-            "p1_char": "FE",
-            "p2_char": "GA",
-            "winning_char": "",
-            "p1_name": "",
-            "p2_name": "",
-            "timestamp": 346,
-            "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
-            "uploader": "TESTER"
-        },
-        {
-            "id": "73d7d046-0e45-4488-9584-e6893a8a003b",
-            "type": "VI",
-            "url": "https://youtu.be/Cm9UTXJZgzw",
-            "p1_char": "FE",
-            "p2_char": "GA",
-            "winning_char": "",
-            "p1_name": "",
-            "p2_name": "",
-            "timestamp": 397,
-            "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
-            "uploader": "TESTER"
-        }
     ]
 }
 
@@ -92,7 +50,7 @@ const secondPage = {
             "p2_name": "",
             "timestamp": 477,
             "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
+            "video_title": "PAGE2",
             "uploader": "TESTER"
         },
         {
@@ -106,51 +64,9 @@ const secondPage = {
             "p2_name": "",
             "timestamp": 564,
             "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
+            "video_title": "PAGE2",
             "uploader": "TESTER"
         },
-        {
-            "id": "b3a4309a-9960-4077-bed6-38c0d2f895a7",
-            "type": "VI",
-            "url": "https://youtu.be/Cm9UTXJZgzw",
-            "p1_char": "QB",
-            "p2_char": "GA",
-            "winning_char": "",
-            "p1_name": "",
-            "p2_name": "",
-            "timestamp": 674,
-            "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
-            "uploader": "TESTER"
-        },
-        {
-            "id": "7ded4d67-2a50-431a-a58d-3f2d893b9439",
-            "type": "VI",
-            "url": "https://youtu.be/Cm9UTXJZgzw",
-            "p1_char": "QB",
-            "p2_char": "GA",
-            "winning_char": "",
-            "p1_name": "",
-            "p2_name": "",
-            "timestamp": 776,
-            "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
-            "uploader": "TESTER"
-        },
-        {
-            "id": "910b718c-c6ca-4f67-8a5f-ece6d0c98f06",
-            "type": "VI",
-            "url": "https://youtu.be/Cm9UTXJZgzw",
-            "p1_char": "QB",
-            "p2_char": "GA",
-            "winning_char": "",
-            "p1_name": "",
-            "p2_name": "",
-            "timestamp": 884,
-            "date_uploaded": "2020-12-26T00:00:00Z",
-            "video_title": "",
-            "uploader": "TESTER"
-        }
     ]
 }
 
